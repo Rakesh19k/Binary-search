1d0f7731d81873557ce2096bd4233ce2a1629c2b
Rakesh19k <58321558+Rakesh19k@users.noreply.github.com>
2020-02-07 08:35:19 +0530

    Update Binarysearch.py

--- Binarysearch.py
+++ Binarysearch.py
@@ -1,4 +1,4 @@
-pos=-1
+position=-1
 def search(list1,n):
 	low=0
 	high=len(list1)-1
@@ -6,14 +6,14 @@ def search(list1,n):
 	while low<=high and not Found:
 		mid = (low+high)//2
 		if list1[mid] == n:
-			globals()["pos"] = mid
+			globals()["position"] = mid
 			Found = True
 		elif list1[mid]<n:
 			low=mid+1
 		else:
 			high=mid-1
 	if Found==True:
-		print ("Found at and position is", pos+1)
+		print ("Found at and position is", position+1)
 	else:
 		print ("Not Found")
 list1=[89465,4,7,8,12,45,99,1236,89510,57469]
